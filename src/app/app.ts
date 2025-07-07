8fcdebbd03df4bc8f02e5a32eb7f626740320d8c
CamiloAcevedoCuevas <ccuevasa@uninorte.edu.co>
2025-07-06 20:16:23 -0500

    reactive forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from './shared/components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'reactive-forms-app';
